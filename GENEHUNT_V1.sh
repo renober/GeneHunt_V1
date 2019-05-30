8458c4ff8d2ce30c794103311c5c8bbd28f3026a
renober <46741660+renober@users.noreply.github.com>
2019-05-30 13:54:39 -0700

    Update GENEHUNT_V1.sh

--- GENEHUNT_V1.sh
+++ GENEHUNT_V1.sh
@@ -44,17 +44,28 @@ else
 	echo -e \#Dom. of interest\: >>$OUTFILE2.txt
 	cat $PFDom | grep -v "#" | sed 's/^/#/g' >>$OUTFILE2.txt
 	echo "######################################################################################################" >>$OUTFILE2.txt
-	echo -e Target"\t"PFam"\t"PFlen"\t"Query"\t"Qlen"\t"DomEval"\t"PFfrom"\t"PFto"\t"Qfrom"\t"Qto"\t"AliRatio >>$OUTFILE2.txt
-	hmmsearch --domtblout .POTHit.txt -E 10e-1 .db1 $SEQFILE
+	echo -e PFam"\t"PFlen"\t"Query"\t"Qlen"\t"DomEval"\t"PFfrom"\t"PFto"\t"Qfrom"\t"Qto"\t"AliRatio >>$OUTFILE2.txt
+	hmmsearch --domtblout .POTHit.txt -E 10e-3 .db1 $SEQFILE
 	grep -v "#" .POTHit.txt | cut -d " " -f 1 | sort -k 1 | uniq | grep -v "^$" | sed 's/$/@/g' > .POTHit2.txt
 		if [[ -s .POTHit2.txt ]]; then
 		grep -F -f .POTHit2.txt .SEQFILE | tr '@' '\n' >.POTHit3.txt
-		hmmscan --domtblout .HITSTAT.txt Pfam-A.hmm .POTHit3.txt
-		grep -F -f $PFDom .HITSTAT.txt | sed 's/\ \ */\t/g' | cut -f 4 | sort -k 1 | uniq > .POTHit4.txt
-		grep -F -f .POTHit4.txt .HITSTAT.txt | awk '{print $1,$2,$3,$4,$6,$13,$16,$17,$18,$19}' | sed 's/ /\t/g' | sort -k 3 -k 9n -k 10n | perl -e 'while(<>) {chomp;@a=split;next if $a[-1]==$a[-2];push(@{$b{$a[2]}},$_);} foreach(sort keys %b){@a=@{$b{$_}};for($i=0;$i<$#a;$i++){@b=split(/\t/,$a[$i]);@c=split(/\t/,$a[$i+1]);$len1=$b[-1]-$b[-2];$len2=$c[-1]-$c[-2];$len3=$b[-1]-$c[-2];if($len3>0 and ($len3/$len1>0.5 or $len3/$len2>0.5)){if($b[4]<$c[4]){splice(@a,$i+1,1);}else{splice(@a,$i,1);}$i=$i-1;}}foreach(@a){print $_."\n";}}' | perl -e 'while(<>){chomp;@a=split(/\t/,$_);if(($a[-1]-$a[-2])>80){print $_,"\t",($a[-3]-$a[-4])/$a[2],"\n" if $a[5]<=1e-5;}else{print $_,"\t",($a[-3]-$a[-4])/$a[2],"\n" if $a[5]<=1e-3;}}' | awk '$NF>0.3' |sort -k 4 -k 9,10g | sed 's/\./\t/' | cut -f 1,2,4- >>$OUTFILE2.txt
+		hmmscan --domtblout .temp1.4 Pfam-A.hmm .POTHit3.txt
+		cat .temp1.4 | grep -v '^#' | awk '{print $2,$3,$4,$6,$13,$16,$17,$18,$19}' | sed 's/ /\t/g' | sort -k 3,3 -k 8n -k 9n | perl -e 'while(<>){chomp;@a=split;next if $a[-1]==$a[-2];push(@{$b{$a[2]}},$_);}foreach(sort keys %b){@a=@{$b{$_}};for($i=0;$i<$#a;$i++){@b=split(/\t/,$a[$i]);@c=split(/\t/,$a[$i+1]);$len1=$b[-1]-$b[-2];$len2=$c[-1]-$c[-2];$len3=$b[-1]-$c[-2];if($len3>0 and ($len3/$len1>0.5 or $len3/$len2>0.5)){if($b[4]<$c[4]){splice(@a,$i+1,1);}else{splice(@a,$i,1);}$i=$i-1;}}foreach(@a){print $_."\n";}}' | perl -e 'while(<>){chomp;@a=split(/\t/,$_);if(($a[-1]-$a[-2])>80){print $_,"\t",($a[-3]-$a[-4])/$a[1],"\n" if $a[4]<=1e-5;}else{print $_,"\t",($a[-3]-$a[-4])/$a[1],"\n" if $a[4]<=1e-3;}}' | awk '$NF>0.3' |sort -k 3 -k 8,9g | grep -F -f $PFDom > .temp1.5
+		#set the evalue (by default 1e-3) and the HMM converage (by default >30%)
+
+		cut -f 3 .temp1.5  | sort -k 1 | uniq > .POTHit4.txt
+
+		if [[ -s .temp1.5 ]]; then
+		cat .temp1.4 | grep -v '^#' | awk '{print $2,$3,$4,$6,$13,$16,$17,$18,$19}' | sed 's/ /\t/g' | sort -k 3,3 -k 8n -k 9n | perl -e 'while(<>){chomp;@a=split;next if $a[-1]==$a[-2];push(@{$b{$a[2]}},$_);}foreach(sort keys %b){@a=@{$b{$_}};for($i=0;$i<$#a;$i++){@b=split(/\t/,$a[$i]);@c=split(/\t/,$a[$i+1]);$len1=$b[-1]-$b[-2];$len2=$c[-1]-$c[-2];$len3=$b[-1]-$c[-2];if($len3>0 and ($len3/$len1>0.5 or $len3/$len2>0.5)){if($b[4]<$c[4]){splice(@a,$i+1,1);}else{splice(@a,$i,1);}$i=$i-1;}}foreach(@a){print $_."\n";}}' | perl -e 'while(<>){chomp;@a=split(/\t/,$_);if(($a[-1]-$a[-2])>80){print $_,"\t",($a[-3]-$a[-4])/$a[1],"\n" if $a[4]<=1e-5;}else{print $_,"\t",($a[-3]-$a[-4])/$a[1],"\n" if $a[4]<=1e-3;}}' | awk '$NF>0.3' |sort -k 3 -k 8,9g  >>$OUTFILE2.txt
+		#set the evalue (by default 1e-3) and the HMM converage (by default >30%)
+		
+		########
+		
 		grep -F -f .POTHit4.txt .SEQFILE | tr '@' '\n' > $OUTFILE3.txt
 		else
 	echo NO HIT\!
 	fi
 fi
+fi
+
 rm -r  .POTHit*  .SEQFILE .db1 .HITSTAT.txt
